ef649166fc910393e5dfaf76e67537baceb73a19
Vasya <vasiliagavriluk924@gmail.com>
2022-12-20 19:56:10 +0300

    modified:   bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php 	deleted:    bitrix/cache/s1/bitrix/news.list/e25/a8/a8a2a3c9cd639339f452a55276183942.php

--- bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
+++ bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001671554459';
-$dateexpire = '001674146459';
+$datecreate = '001671555206';
+$dateexpire = '001674147206';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";N;}';
 return true;
 ?>
